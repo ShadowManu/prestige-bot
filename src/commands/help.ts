0204e0f65c141cc0148e62848cac2498e7c96f5b
Cristian <cmedina270793@gmail.com>
2018-03-28 00:21:57 -0400

    Added import dedent

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,3 +1,4 @@
+import * as dedent from 'dedent';
 import { getRepository } from 'typeorm';
 import * as Tg from 'node-telegram-bot-api';
 
@@ -6,12 +7,11 @@ export const HELP_REGEX = /^\/help$/;
 export async function help(bot: Tg) {
 
     const message = dedent`
-			<b>Commands</b>
-			<b>/listprestige</b> : shows all user's current amount of prestige.
-			<b>/prestigeregister</b> : it register yourself.
-			<b>/sendprestige</b> [user, amount] : it transfers the specified amount to the user.
-			<b>/showprestige</b> : shows the your amount of prestige.
-		`;
+		<b>Commands</b>
+		<b>/listprestige</b> : shows all user's current amount of prestige.
+		<b>/prestigeregister</b> : it register yourself.
+		<b>/sendprestige</b> [user, amount] : it transfers the specified amount to the user.
+		<b>/showprestige</b> : shows the your amount of prestige.
+	`;
     await bot.sendMessage(chatId, message, { parse_mode: 'html'});
-
 }

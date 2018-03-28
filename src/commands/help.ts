e31e58ba9b8ec24d9ee256b3b3dd9f99d80ddc51
Cristian <cmedina270793@gmail.com>
2018-03-28 00:25:29 -0400

    added missing chatID Ooooopss

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,13 +5,14 @@ import * as Tg from 'node-telegram-bot-api';
 export const HELP_REGEX = /^\/help$/;
 
 export async function help(bot: Tg) {
-
+	
+    const chatId = msg.chat.id;
     const message = dedent`
-		<b>Commands</b>
-		<b>/listprestige</b> : shows all user's current amount of prestige.
-		<b>/prestigeregister</b> : it register yourself.
-		<b>/sendprestige</b> [user, amount] : it transfers the specified amount to the user.
-		<b>/showprestige</b> : shows the your amount of prestige.
-	`;
+        <b>Commands</b>
+        <b>/listprestige</b> : shows all user's current amount of prestige.
+        <b>/prestigeregister</b> : it register yourself.
+        <b>/sendprestige</b> [user, amount] : it transfers the specified amount to the user.
+        <b>/showprestige</b> : shows the your amount of prestige.
+    `;
     await bot.sendMessage(chatId, message, { parse_mode: 'html'});
 }

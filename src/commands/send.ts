febe6021dd0ca84aca752d43e4f30e43b4a0ff84
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-04-10 19:45:50 -0400

    update send command

--- src/commands/send.ts
+++ src/commands/send.ts
@@ -1,51 +1,43 @@
-import { getRepository } from 'typeorm';
+import * as dedent from 'dedent';
 import * as Tg from 'node-telegram-bot-api';
-import { isNil } from 'lodash';
+import { toNumber } from 'lodash';
+import { getRepository } from 'typeorm';
 
 import { User } from '../entity';
+import { referUser, PRESTIGE_SYMBOL as PS } from '../helpers';
+import { registerUser } from './register';
 
 export const SEND_REGEX = /^\/send@TheRealPrestigeBot @(\S+) (\d+)$/;
 
 export async function sendPrestige(bot: Tg, msg: Tg.Message, match: RegExpExecArray) {
-  const chatId = msg.chat.id;
-  const fromUsername = msg.from!.username!;
+  async function send(message: string, opts?: Tg.SendMessageOptions) {
+    return bot.sendMessage(msg.chat.id, message, opts);
+  }
 
   // tslint:disable:no-magic-numbers
-  const toUsername = match[1];
-  const quantity = +match[2];
+  const username = match[1];
+  const quantity = toNumber(match[2]) || 0;
   // tslint:enable
 
-  const frm = await getRepository(User).findOneById(fromUsername);
-  const to = await getRepository(User).findOneById(toUsername);
+  const frm = await registerUser(bot, msg, false);
+  const to = await getRepository(User).findOne({ username });
 
-  if (fromUsername === toUsername) {
-    await bot.sendMessage(chatId, `Don't send yourself prestige @${fromUsername}, you dickhead.`);
-    return;
-  }
-
-  if (isNil(frm)) {
-    await bot.sendMessage(chatId, `You are not registered.`);
-    return;
-  }
-
-  if (isNil(to)) {
-    await bot.sendMessage(chatId, `User ${toUsername} is not registered.`);
-    return;
-  }
-
-  if (frm.prestige < quantity) {
-    await bot.sendMessage(chatId, `You do not have enough prestige.`);
-    return;
-  }
+  // Verify all conditions for failure
+  if (!frm) return send(`You are not registered.`);
+  if (!to) return send(`Couldn't find ${username} in the system.`);
+  if (frm.id === to.id) return send(`Don't send yourself prestige ${referUser(frm)}, you dickhead.`);
+  if (quantity <= 0) return send(`You can only send positive quantities, ${referUser(frm)}.`);
+  if (frm.prestige < quantity) return send(`You do not have enough prestige, ${referUser(frm)}.`);
 
   const fromPrestige = frm.prestige - quantity;
   const toPrestige = to.prestige + quantity;
 
-  await getRepository(User).save({ username: fromUsername, prestige: fromPrestige });
-  await getRepository(User).save({ username: toUsername, prestige: toPrestige });
+  await getRepository(User).save({ id: frm.id, prestige: fromPrestige });
+  await getRepository(User).save({ id: to.id, prestige: toPrestige });
 
-  await bot.sendMessage(chatId, [
-    `User ${fromUsername} now has ${fromPrestige},`,
-    `and user ${toUsername} now has ${toPrestige}`
-  ].join(' '));
+  return send(dedent`
+    ${referUser(frm)} (\u2b06${fromPrestige}️)
+        has gifted <b>${quantity} ${PS}</b> to
+    ${referUser(to)} (\u2b07${toPrestige})
+  `, { parse_mode: 'html' });
 }

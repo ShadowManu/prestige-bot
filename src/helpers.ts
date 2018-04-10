28f1eafc803aa5681d99a4a9feb36151686be430
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-04-10 19:45:34 -0400

    add more helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,10 +4,12 @@ import { toString } from 'lodash';
 
 import { User } from './entity';
 
+export const PRESTIGE_SYMBOL = 'PrestigeCoins™';
+
 export function referTgUser(user: Tg.User): string {
   return (user.username && `@${user.username}`) || user.first_name || toString(user.id);
 }
 
-export function referUser(user: User): string {
-  return (user.username && `@${user.username}`) ||  toString(user.id);
+export function referUser(user: User, mention = true): string {
+  return (user.username && `${mention && '@'}${user.username}`) ||  toString(user.id);
 }

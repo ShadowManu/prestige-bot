cd04c4371f338d3021de8801587ffbf25eb768e1
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-04-10 18:35:06 -0400

    update register command

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,28 +1,41 @@
+import * as dedent from 'dedent';
 import { getRepository } from 'typeorm';
 import * as Tg from 'node-telegram-bot-api';
 import { isNil } from 'lodash';
 
 import { User } from '../entity';
+import { referTgUser } from '../helpers';
 
 const BASE_PRESTIGE = 50;
 
 export const REGISTER_REGEX = /^\/register@TheRealPrestigeBot$/;
 
-export async function registerUser(bot: Tg, msg: Tg.Message) {
-  const chatId = msg.chat.id;
-  const username = msg.from!.username!;
+export async function registerUser(bot: Tg, msg: Tg.Message, interactive: boolean): Promise<User | undefined> {
+  async function send(message: string) { return bot.sendMessage(msg.chat.id, message); }
 
-  const existing = await getRepository(User).findOneById(username);
-
-  let message: string;
-
-  if (!isNil(existing)) {
-    message = `@${username}, you are already registered.`;
-
-  } else {
-    await getRepository(User).save({ username, prestige: BASE_PRESTIGE });
-    message = `@${username}, you have been registered sucessfully.`;
+  if (!msg.from) {
+    if (interactive) await send(dedent`
+      Couldn't register user as the message doesn't have a sender.
+      Contact the developers if you think that's an error.`
+    );
+    return;
   }
 
-  await bot.sendMessage(chatId, message);
+  const id = msg.from.id;
+  const username = msg.from.username;
+  const reference = referTgUser(msg.from);
+
+  const existing = await getRepository(User).findOneById(id);
+
+  // New user
+  if (isNil(existing)) {
+    const incoming = await getRepository(User).save({ id, username, prestige: BASE_PRESTIGE });
+    if (interactive) await send(`${reference}, welcome to the one and only, TheRealPrestigeBot.`);
+    return incoming;
+
+  // Existing user
+  } else  {
+    if (interactive) await send(`${reference}, we already track you, don't worry ;).`);
+    return existing;
+  }
 }

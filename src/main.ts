0fc44725d20c8ed36cddd64874bd6b26c47f599b
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-04-10 22:14:48 -0400

    fix(app): add basic rejection handler

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,15 @@ async function bootstrap() {
   bot.onText(c.REGISTER_REGEX, async msg => c.registerUser(bot, msg, true));
   bot.onText(c.SEND_REGEX, async(msg, match) => c.sendPrestige(bot, msg, match!));
   bot.onText(c.SHOW_REGEX, async msg => c.showPrestige(bot, msg));
+
+  // Logs unhandled errors to standard error
+  process.on('unhandledRejection', (err, promise) => {
+    // tslint:disable:no-console
+    console.error('Unhandled rejection at:', new Date());
+    console.error('promise: ', promise);
+    console.error('reason: ', err);
+    // tslint:enable
+  });
 }
 
 bootstrap()

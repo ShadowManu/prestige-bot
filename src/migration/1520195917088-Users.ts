cbfe3c2a68895e86056c58cd815a786d58be1bda
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-04-15 16:10:24 -0400

    fix user id integer type size

--- src/migration/1520195917088-Users.ts
+++ src/migration/1520195917088-Users.ts
@@ -5,7 +5,7 @@ export class User1520195917088 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE TABLE users (
-        id integer PRIMARY KEY,
+        id bigint PRIMARY KEY,
         username text,
         prestige integer NOT NULL
       );
